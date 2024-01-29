8a5912ff4e5d66992447a578473bbce8e6c50d70
KoranitDokpromla <122878142+KoranitDokpromla@users.noreply.github.com>
2024-01-29 18:03:40 +0700

    init

---
+++ src/config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary");
+          
+cloudinary.config({ 
+  cloud_name: 'dqfaydmwu', 
+  api_key: '785956433523951', 
+  api_secret: 'oEH-Q0MJIhDz-F0JjmiZxNBffhE' 
+});
+
+  module.exports = cloudinary;
